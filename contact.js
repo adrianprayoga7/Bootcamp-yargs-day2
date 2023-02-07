43464bd7799496c821db5e9a17d5a13abf5e1c52
Andrian Prayoga <adrianprayoga7@gmail.com>
2023-02-07 15:24:06 +0700

    Materi mengenai yargs

---
+++ contact.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const validator = require('validator');
+
+//membuat dan mengecek folder bernama data
+if (!fs.existsSync('data')) {
+  fs.mkdirSync('data');
+}
+
+//membuat dan mengecek file bernama contacts.json
+if (!fs.existsSync('data/contacts.json')) {
+  fs.writeFileSync ('data/contacts.json', '[]', 'utf-8');
+}
+
+
+const answer = (nama,tlp,email) => {
+  const contact = (nama,tlp,email);
+  const file = fs.readFileSync('data/contacts.json', 'utf-8');
+  const contacts = JSON.parse(file);
+
+//untuk validasi email jika yang di inputkan bukan format email
+const validasiEmail = validator.isEmail(contact.email);
+if (validasiEmail == false) {
+  console.log("email tidak valid");
+  return false;
+}
+
+//untuk validasi notlp jika yang diinputkan bukan format id
+const validasiTlp = validator.isMobilePhone(contact.tlp, 'id-ID');
+if (validasiTlp == false) {
+  console.log("Nomor Telepon tidak valid");
+  return false;
+}
+
+//tujuannya untuk push ke contacts.json apa yang sudah diinputkan
+contacts.push(contact);
+fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+console.log('Terima kasih');
+
+};
+
+module.exports = {answer};
+
+
+
+  
