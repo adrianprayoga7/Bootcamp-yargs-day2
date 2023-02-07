d39e063d8bd2b12f1463818d441121dacf21363a
Andrian Prayoga <adrianprayoga7@gmail.com>
2023-02-07 15:46:44 +0700

    materi mengenai yargs

--- contact.js
+++ contact.js
@@ -11,18 +11,20 @@ if (!fs.existsSync('data/contacts.json')) {
   fs.writeFileSync ('data/contacts.json', '[]', 'utf-8');
 }
 
-
+//tujuannya untuk parsing data dari nama,tlp,email ke contacts.json
 const answer = (nama,tlp,email) => {
-  const contact = (nama,tlp,email);
+  const contact = {nama,tlp,email};
   const file = fs.readFileSync('data/contacts.json', 'utf-8');
   const contacts = JSON.parse(file);
 
+
 //untuk validasi email jika yang di inputkan bukan format email
+
 const validasiEmail = validator.isEmail(contact.email);
 if (validasiEmail == false) {
   console.log("email tidak valid");
   return false;
-}
+};
 
 //untuk validasi notlp jika yang diinputkan bukan format id
 const validasiTlp = validator.isMobilePhone(contact.tlp, 'id-ID');
